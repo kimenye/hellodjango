68948377ea83ea4d45f2f40dee630d799a29d9d4
Trevor Kimenye <kimenye@gmail.com>
2013-03-20 14:43:36 +0300

    made the urls compartmentalized

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('tivi.views',
-	url(r'^tivi/$', 'index'),
-	url(r'^tivi/(?P<show_id>\d+)/$', 'detail'),
-)
+# urlpatterns = patterns('tivi.views',
+# 	url(r'^tivi/$', 'index'),
+# 	url(r'^tivi/(?P<show_id>\d+)/$', 'detail'),
+# )
 
-urlpatterns += patterns('',
+urlpatterns = patterns('',
+	url(r'^tivi/', include('tivi.urls')),
     url(r'^admin/', include(admin.site.urls)),		
 )

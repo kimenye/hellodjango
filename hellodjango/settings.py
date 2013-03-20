413f9855c7d496681cb0dee8e9a0c8cb98d796b9
Trevor Kimenye <kimenye@gmail.com>
2013-03-20 13:10:22 +0300

    added new tivi app with show models

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -119,12 +119,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'tivi',
     # 'south'
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
---
+++ tivi/admin.py
@@ -0,0 +1,4 @@
+from tivi.models import Show
+from django.contrib import admin
+
+admin.site.register(Show)
